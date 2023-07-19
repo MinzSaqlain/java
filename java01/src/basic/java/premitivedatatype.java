aad9d1dc992a9e5e3db1c3d2c4f72a5e7e0cff29
SaqlainMinz <officialsaqlainminz98@gmail.com>
2023-07-19 21:11:47 +0530

    updated

---
+++ java01/src/basic/java/premitivedatatype.java
@@ -0,0 +1,26 @@
+package basic.java;
+
+public class premitivedatatype {
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static char ch;
+	static boolean bb;
+	
+	
+	public static void main(String[] args) {
+		System.out.println(premitivedatatype.b); //0
+		System.out.println(premitivedatatype.s); //0
+		System.out.println(premitivedatatype.i); //0
+		System.out.println(premitivedatatype.l);//0
+		System.out.println(premitivedatatype.f);//0.0
+		System.out.println(premitivedatatype.d);//0.0
+		System.out.println(premitivedatatype.ch);
+		System.out.println(premitivedatatype.bb);// false 
+		//primitive data types shows all the default value 
+		
+	}
+}

bc007b3599c529c5279ffaebda86336cb8d927e1
SaqlainMinz <officialsaqlainminz98@gmail.com>
2023-07-19 21:18:42 +0530

    updated

---
+++ java01/src/basic/java/wrapperClass.java
@@ -0,0 +1,25 @@
+package basic.java;
+
+public class wrapperClass {
+	static Byte b;
+	static Short s;
+	static Integer i;
+	static Long l;
+	static Float f;
+	static Double d;
+	static Character ch;
+	static Boolean bb;
+
+	public static void main(String[] args) {
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(ch);
+		System.out.println(bb);
+			// in wrapper class it shows the output as null 
+	}
+
+}
